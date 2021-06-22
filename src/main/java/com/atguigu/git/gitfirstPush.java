65392bd66bcc74b8917f9cc2984a040b91ff9619
lhx1119 <85397421+lhx1119@users.noreply.github.com>
2021-06-22 21:10:05 +0800

    Update gitfirstPush.java

--- src/main/java/com/atguigu/git/gitfirstPush.java
+++ src/main/java/com/atguigu/git/gitfirstPush.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class gitfirstPush {
     public static void main(String[] args) {
         System.out.println("第一次push！");
+        System.out.println("第一次pull！");
     }
 }
